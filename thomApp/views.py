5ece142a35b6dea3ae8bebdfe554a60f02e848d4
lek sitha <sitha.lek@gmail.com>
2025-09-29 05:08:19 +0700

    First deploy

---
+++ thomApp/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render,HttpResponse
+import requests
+from django.http import JsonResponse
+
+
+
+def home(request):
+    # return render(request,'thomApp/index.html')
+    thom='battambang'
+    API_Key='3aef1df689e335c34f01af97a72a88f4'
+    url='https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid={}'
+    response=requests.get(url.format(thom,API_Key)).json()
+    
+    return JsonResponse(response)
+    # return HttpResponse(response['name'])
+def test(request):
+    return HttpResponse("My Test")
