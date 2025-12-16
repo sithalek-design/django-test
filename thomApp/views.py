d37bafb2fe10d0c8c29e121baa84ca4a36c9eaeb
Sitha Lek <sitha.lek@gmail.com>
2025-12-16 22:17:54 +0700

    thom

--- thomApp/views.py
+++ thomApp/views.py
@@ -14,4 +14,4 @@ def home(request):
     return JsonResponse(response)
     # return HttpResponse(response['name'])
 def test(request):
-    return HttpResponse("My Test Sitha")
+    return HttpResponse("My Test Thom")
